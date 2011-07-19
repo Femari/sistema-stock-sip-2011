90f9689cbe85f43518b835a8789f8bd7026783f6
daniel.datri@gmail.com <daniel.datri@gmail.com@22b514ed-9321-4a66-9b4e-6242fc57cb00>
2011-07-19 22:33:57 +0000

    Modificando el telefono y el fax a String

--- trunk/SIPGUI/src/Negocio/Modelo/Cliente.java
+++ trunk/SIPGUI/src/Negocio/Modelo/Cliente.java
@@ -1,56 +1,67 @@
 package Negocio.Modelo;
+
 public class Cliente {
-	//PrimaryKey
+    //PrimaryKey
+
     private long cuit;
     private String nombre;
     private String direccion;
     private String codigoPostal;
-    private int telefono;
-    private int fax;
-	
-    public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public long getCuit() {
-		return cuit;
-	}
-	public void setCuit(long cuit) {
-		this.cuit = cuit;
-	}
-	public String getDireccion() {
-		return direccion;
-	}
-	public void setDireccion(String calle) {
-		this.direccion = calle;
-	}
-	public String getCodigoPostal() {
-		return codigoPostal;
-	}
-	public void setCodigoPostal(String codigoPostal) {
-		this.codigoPostal = codigoPostal;
-	}
-	public int getTelefono() {
-		return telefono;
-	}
-	public void setTelefono(int telefono) {
-		this.telefono = telefono;
-	}
-	public int getFax() {
-		return fax;
-	}
-	public void setFax(int fax) {
-		this.fax = fax;
-	}
-        
-        public boolean isEmpty(){
-            
-            return (this.codigoPostal.isEmpty() && this.cuit == 0 && 
-                    this.direccion.isEmpty() && this.fax == 0 && this.nombre.isEmpty() && 
-                    this.telefono == 0)?true:false;
-        }
+    private String telefono;
+    private String fax;
 
-    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public long getCuit() {
+        return cuit;
+    }
+
+    public void setCuit(long cuit) {
+        this.cuit = cuit;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String calle) {
+        this.direccion = calle;
+    }
+
+    public String getCodigoPostal() {
+        return codigoPostal;
+    }
+
+    public void setCodigoPostal(String codigoPostal) {
+        this.codigoPostal = codigoPostal;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+
+    public boolean isEmpty() {
+
+        return (this.codigoPostal.isEmpty() && this.cuit == 0
+                && this.direccion.isEmpty() && this.fax.isEmpty() && this.nombre.isEmpty()
+                && this.telefono.isEmpty()) ? true : false;
+    }
 }
