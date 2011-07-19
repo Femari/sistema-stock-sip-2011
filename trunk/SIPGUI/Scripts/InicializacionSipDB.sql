5f59b39be6eb2d4e9ab5d34d94b3cb11509d473b
daniel.datri@gmail.com <daniel.datri@gmail.com@22b514ed-9321-4a66-9b4e-6242fc57cb00>
2011-07-19 23:01:55 +0000

    Se agrego la columna de habilitado al cliente y se modifico el insert al cliente

--- trunk/SIPGUI/Scripts/InicializacionSipDB.sql
+++ trunk/SIPGUI/Scripts/InicializacionSipDB.sql
@@ -13,12 +13,12 @@ DELETE Proveedor
 
 GO
 
-INSERT INTO Cliente(Cuit, Nombre, Direccion, CodigoPostal, Telefono, Fax) VALUES (1, 'QuieroMats S.A.', 'Amenabar 2045', '1429', 5551234, 5551234)
-INSERT INTO Proveedor(Cuit, Nombre, Direccion, CodigoPostal, Telefono, Fax) VALUES (1, 'DoyMats SRL', 'Juana Azurduy 1366', '1420', 5556789, 5556789)
+INSERT INTO Cliente(Cuit, Nombre, Direccion, CodigoPostal, Telefono, Fax, Habilitado) VALUES (1, 'QuieroMats S.A.', 'Amenabar 2045', '1429','5551234','5551234',1)
+INSERT INTO Proveedor(Cuit, Nombre, Direccion, CodigoPostal, Telefono, Fax) VALUES (1, 'DoyMats SRL', 'Juana Azurduy 1366', '1420','5556789','5556789')
 
-INSERT INTO Producto(IdProducto, Nombre, Descripcion, IdProveedor, PrecioCompra, StockMinimo) VALUES ('PROD1', 'Caños', 'Caños... largos', 1, 100.50, 20)
+INSERT INTO Producto(IdProducto, Nombre, Descripcion, IdProveedor, PrecioCompra, StockMinimo) VALUES ('PROD1', 'Canios', 'Canios... largos', 1, 100.50, 20)
 INSERT INTO Producto(IdProducto, Nombre, Descripcion, IdProveedor, PrecioCompra, StockMinimo) VALUES ('PROD2', 'Tuercas', 'Tuercas de acero cromado', 1, 1.50, 100)
-INSERT INTO Producto(IdProducto, Nombre, Descripcion, IdProveedor, PrecioCompra, StockMinimo) VALUES ('PROD3', 'Tornillos', 'Tornillos de 2" métrico', 1, 1.25, 150)
+INSERT INTO Producto(IdProducto, Nombre, Descripcion, IdProveedor, PrecioCompra, StockMinimo) VALUES ('PROD3', 'Tornillos', 'Tornillos de 2" mï¿½trico', 1, 1.25, 150)
 
 INSERT INTO StockDeposito(IdProducto, Cantidad) VALUES ('PROD1', 500)
 INSERT INTO StockDeposito(IdProducto, Cantidad) VALUES ('PROD2', 500)
